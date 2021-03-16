0e85d2c14417a163454d58e637bbc6a70561d8bd
benogit <benporter95@protonmail.com>
2021-03-16 17:02:03 +1000

    Done With Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    
+    return changeText()
+});
+
+function changeText(){
+    document.querySelector("p").textContent="This is really cool!"
+};
